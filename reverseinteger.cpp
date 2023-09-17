9a840fb889aa46cfd011a4d9961f741f40c53243
Ashish Sharma <120502044+Ashishbsharma@users.noreply.github.com>
2023-09-16 17:35:30 -0700

    Add files via upload

---
+++ reverseinteger.cpp
@@ -0,0 +1,17 @@
+ // Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+       int y=0;
+       while(x!=0)
+       {
+       int rev= x%10;
+       x=x/10;
+       y= y*10+rev;
+       }
+ 
+      cout<<y<<endl;
+}
